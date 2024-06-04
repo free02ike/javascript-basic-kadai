e0e9a070afb9efa923dfa61dcc5f2a35c31a6776
free02ike <164166214+free02ike@users.noreply.github.com>
2024-06-04 16:40:55 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const text = document.getElementById('text');
-const btn = document.getElementById('btn');
-btn.addEventListener('click',()=>{
-  const textSecond = document.createElement('h2');
-  textSecond.textContent = 'ボタンをクリックしました';
-  document.body.appendChild(textSecond); // 新しい要素をbodyに追加
-});
